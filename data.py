e83b55dac507bff9868ae660f9cf3849a4af97ed
Jatin Madan <jatin.madan39@gmail.com>
2019-07-25 15:35:10 +0530

    Initial Commit

---
+++ data.py
@@ -0,0 +1,8 @@
+git add .import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pylab as plt
+import seaborn as sns
+import warnings
+warnings.simplefilter("ignore")
+plt.style.use('ggplot')
+color_pal = [x['color'] for x in plt.rcParams['axes.prop_cycle']]
